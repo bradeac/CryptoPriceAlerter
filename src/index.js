67de6f140bd8f1e1db6234346489e46b2289218f
bradeac <rs6avant>
2018-06-14 23:20:06 +0300

    try catch block

--- src/index.js
+++ src/index.js
@@ -16,16 +16,21 @@ const pusher = new PushBullet(process.env.PUSHBULLET_URL)
 
 const main = async () => {
     cron.schedule('* * * * *', async () => {
-        let PCNBTC = await fetch('PCN/BTC')
+        try {
+            let PCNBTC = await fetch('PCN/BTC')
 
-        if (PCNBTC.ask <= '0.00000014') {
-            pusher.note(
-                process.env.PUSHBULLET_DEVICE_ID,
-                'Peepcoin',
-                `Obiect: ${JSON.stringify(PCNBTC, null, 4)}`,
-                (error, response) => {
-                    console.log('Sent notification')
-            });
+            if (PCNBTC.ask <= '0.00000014') {
+                pusher.note(
+                    process.env.PUSHBULLET_DEVICE_ID,
+                    'Peepcoin',
+                    `Obiect: ${JSON.stringify(PCNBTC, null, 4)}`,
+                    (error, response) => {
+                        console.log('Sent notification')
+                    }
+                );
+            }
+        } catch (error) {
+            console.log('Error: ', error)
         }
     })
 }
